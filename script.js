491ec1c7ca929db251df01330c272ede3994e2ff
George McRedmond <georgem.int101@gmail.com>
2018-06-29 09:19:41 -0400

    Improved Game

--- script.js
+++ script.js
@@ -1,57 +1,42 @@
-function troll_fight () {
-	var troll = prompt("You're walking through the forest, minding your own business, and you run into a troll! Do you FIGHT him, PAY him or RUN?").toUpperCase();
+"use strict";
+
+function trollBattle() {
+	var action = window.prompt("You're walking through a forest, minding your own business, and suddenly a troll appears! Do you FIGHT the troll, BRIBE the troll or RUN for your life?").toUpperCase();
 	
-	switch(troll) {
+	//switch statement to handle initial action
+	switch(action) {
 		case "FIGHT":
-		var strong = prompt("How brave! Are you strong? (YES or NO)").toUpperCase();
-		var smart = prompt("Are you smart? (YES or NO)").toUpperCase();
-		if(strong === "YES" || smart === "YES") {
-			document.getElementById("result").innerHTML="You only need one of the two! You defeat the troll, good job!";
-		} else {
-			document.getElementById("death").innerHTML="You're not strong or smart? <br>Guess you shouldn't have fought a troll. <br>You have died.";
-		}
+			var strong = window.prompt("Wow, how brave! Are you strong? (YES or NO)").toUpperCase();
+			var smart = window.prompt("Are you smart? (YES or NO)").toUpperCase();
+			if(strong === "YES" || smart === "YES") {
+				document.getElementById("result").innerHTML="You only need to be either stronger or smarter to beat a troll.</br> You win, good job!";
+			} else {
+				document.getElementById("death").innerHTML="You're not strong or smart? </br> I guess you shouldn't have fought a troll.</br> You have died.";
+			}
 		break;
 		
-		case "PAY":
-		var money = prompt("Do you have any money? (YES or NO)").toUpperCase();
-		var dollars = prompt("Is your money in troll dollars? (YES or NO)").toUpperCase();
-		if (money==="YES" && dollars==="YES") {
-			document.getElementById("result").innerHTML="Great! You have paid the troll and live to tell about it!";
-		} else {
-			document.getElementById("death").innerHTML="Uh-oh! This troll only accepts troll dollars!! <br>You have died!";
-		}
+		case "BRIBE":
+			var money = window.prompt("You have to pay the troll toll, do you have any money? (YES or NO)").toUpperCase();
+			var dollars = window.prompt("This troll only accepts troll bucks. Is your money in troll dollars? (YES or NO)").toUpperCase();
+			if (money === "YES" && dollars === "YES") {
+				document.getElementById("result").innerHTML = "Great job.</br> You have paid the troll and lived to tell about it!";
+			} else {
+				document.getElementById("death").innerHTML = "You fool, you need troll dollars to pay off a troll!</br> You have died!";
+			}
 		break;
 		
 		case "RUN":
-		var fast = prompt("You decide to run. Are you fast? (YES or NO)").toUpperCase();
-		var headStart = prompt("Did you get a head start? (YES or NO)").toUpperCase();
-		if (fast==="YES" || headStart==="YES"){
-			document.getElementById("result").innerHTML="You manage to get away...barely!";
-		} else {
-			document.getElementById("death").innerHTML="You need to be quicker, or get a head start.<br> Guess you'll make a nice troll lunch.";
-		}
+			var speed = window.prompt("Are you fast? (YES or NO)").toUpperCase();
+			if (speed === "YES") {
+				document.getElementById("result").innerHTML = "Your speed allowed you to get away and live another day.</br> But can you still live with yourself?";
+			} else {
+				document.getElementById("death").innerHTML = "You coward, if you choose to run, make sure you're at least faster than a troll. </br> You have died!";
+			}
 		break;
-		default:
-		window.alert("I didn't understand your choice. Hit refresh and try again, this time please type FIGHT, PAY or RUN!");
-	}
+		
+		default: window.alert("Please type FIGHT, BRIBE or RUN to play the game");	
+		trollBattle();
+	} // end of switch statement	
 }
 
-if (window.addEventListener) {
-	window.addEventListener("load", troll_fight, false);
-} else if (window.attachEvent) {
-	window.attachEvent("onload", troll_fight);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("button").addEventListener("click", trollBattle, false);
